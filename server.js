8c3b92c193534f275dd2b43039b99bbc814a697c
Sam De Legge <samd@Sams-MacBook-Pro.local>
2018-05-08 09:52:24 -0700

    Got handlebars working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
+var exphbs  = require('express-handlebars');
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
@@ -26,34 +26,35 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newsdb");
+mongoose.connect((process.env.MONGODB_URI) ? process.env.MONGODB_URI : "mongodb://localhost/newsdb");
 
 // Routes
 
 app.get("/", function (req, res) {
 
   db.Article.find({})
-  .then (function (dbArticle) {
-
-   res.render("home", { article: dbArticle });
-  })
-  .catch(function (err) {
-
-    res.json(err)
-  })
+    .then(function (dbArticle) {
+      res.render("content", { article: dbArticle });
+    })
+    .catch(function (err) {
+      res.json(err)
+    })
 });
 
 
 // A GET route for scraping the echoJS website
-app.post("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with request
-  axios.get("https://www.nytimes.com/").then(function(response) {
+  axios.get("https://www.nytimes.com/").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("h2.story-heading").each(function (i, element) {
       // Save an empty result object
       var result = {};
 
@@ -64,14 +65,14 @@ app.post("/scrape", function(req, res) {
       result.link = $(this)
         .children("a")
         .attr("href");
-console.log(result)
+      console.log(result)
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
           // View the added result in the console
           console.log(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           // If an error occurred, send it to the client
           return res.json(err);
         });
@@ -83,36 +84,36 @@ console.log(result)
 });
 
 // Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
-    .then(function(dbArticle) {
+    .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
-      res.render("home", { article: dbArticle});
+      res.render("home", { article: dbArticle });
     })
-    .catch(function(err) {
+    .catch(function (err) {
       // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
 // Route for grabbing a specific Article by id, populate it with it's note
-app.post("/articles/:id", function(req, res) {
+app.post("/articles/:id", function (req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
- if (req.params.id === "removeall") {
-   db.Article.remove().then(function () {
-     res.end();
-   })
- }
+  if (req.params.id === "removeall") {
+    db.Article.remove().then(function () {
+      res.end();
+    })
+  }
 
- else {
-   db.Article.remove().then(function () {
-     res.end();
-   })
- }
+  else {
+    db.Article.remove().then(function () {
+      res.end();
+    })
+  }
 })
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running at the speed of light on port " + PORT + "!");
 });
